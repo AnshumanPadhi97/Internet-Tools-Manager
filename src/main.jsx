bed30773b6b94468557b783a311cfae7a5d8b755
Anshuman Padhi <liku1997@gmail.com>
2025-03-02 16:24:55 +0530

    added mantine theming

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "@mantine/core/styles.css";
+
+import { MantineProvider, createTheme } from "@mantine/core";
+
+const theme = createTheme({
+  fontFamily: "Roboto",
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <MantineProvider theme={theme} defaultColorScheme="auto">
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
 );
